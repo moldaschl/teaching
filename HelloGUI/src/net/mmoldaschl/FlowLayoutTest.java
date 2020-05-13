19b56945d40992b72f3903a2a4bd78365f50ed97
Markus Moldaschl <markus.moldaschl@gmail.com>
2020-05-13 08:53:30 +0200

    add example for FlowLayout

---
+++ HelloGUI/src/net/mmoldaschl/FlowLayoutTest.java
@@ -0,0 +1,23 @@
+package net.mmoldaschl;
+
+import javax.swing.*;
+
+/**
+ * @author markus.moldaschl
+ * @since 13.05.20
+ */
+public class FlowLayoutTest {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("FlowLayout test");
+        JPanel panel = new JPanel();
+        for (char c = 0; c <= 5; c++) {
+            panel.add(new JButton("Button " + (char) ('A' + c)));
+        }
+        jFrame.add(panel);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setVisible(true);
+    }
+
+}
