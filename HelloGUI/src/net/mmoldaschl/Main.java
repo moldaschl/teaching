afe0317bec1caad3d361bbd4d13b8e29aaf890d2
Markus Moldaschl <markus.moldaschl@gmail.com>
2020-05-13 08:19:44 +0200

    add first version of HelloGUI module

---
+++ HelloGUI/src/net/mmoldaschl/Main.java
@@ -0,0 +1,14 @@
+package net.mmoldaschl;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        JFrame myFrame = new JFrame("My first Java GUI");
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(300, 300);
+        myFrame.setVisible(true);
+        */
+        new MyFrame("My first Java GUI");
+    }
+}
---
+++ HelloGUI/src/net/mmoldaschl/MyFrame.java
@@ -0,0 +1,20 @@
+package net.mmoldaschl;
+
+import javax.swing.*;
+
+/**
+ * @author markus.moldaschl
+ * @since 07.05.20
+ */
+public class MyFrame extends JFrame {
+
+    public MyFrame(String title) {
+        super(title);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        //add(new JButton("my first button"));
+        add(new JTextField("Editier mich. Ich bin ein JTexfield", 60));
+    }
+
+}
